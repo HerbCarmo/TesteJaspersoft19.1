49ce157bc9600c2f59c73aeb232c7660b438d163
HerbCarmo <101847886+HerbCarmo@users.noreply.github.com>
2022-05-11 19:35:42 -0300

    Arquivo de banco de dados exemplo

---
+++ bancodedados.sql
@@ -0,0 +1,25 @@
+-- --------------------------------------------------------
+-- Servidor:                     127.0.0.1
+-- Versão do servidor:           10.1.19-MariaDB - mariadb.org binary distribution
+-- OS do Servidor:               Win32
+-- HeidiSQL Versão:              9.4.0.5130
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Copiando dados para a tabela testejasper.cliente: ~4 rows (aproximadamente)
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+INSERT INTO `cliente` (`id`, `nome`, `cidade`) VALUES
+	(1, 'rafael', 'santo antonio'),
+	(2, 'herbert ', 'crateus'),
+	(3, 'joao gabriel', 'crateus'),
+	(4, 'gisele', 'assis');
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
